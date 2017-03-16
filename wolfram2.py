896c3f8c891433889862c824a8a41461f1cae3d5
Abhinav Anand <abhinavanand1905@gmail.com>
2017-03-16 12:31:33 +0530

    The wolfram|alpha bot.
    A mini bot created in Python using the Wolframalpha API.
    The app_id used in this code has been deactivated, so you will need to get your own.
    Before running this code make sure you have installed the wolframalpha3.0 python module.

---
+++ wolfram2.py
@@ -0,0 +1,37 @@
+#created by Abhinav Anand
+
+import wolframalpha
+
+query=raw_input('Ask me something: ')
+app_id='2V3684-LXELTTTJ9J'
+#This app_id has been deactivated.
+
+client=wolframalpha.Client(app_id)
+
+def wolfram(query):
+    res=client.query(str(query))
+    try:
+       ans=next(res.results).text
+    except:
+       ans='Sorry, I don\'t know that.'
+    return ans
+
+print wolfram(query)
+
+q='y'
+while q=='y':
+   q=raw_input('Wanna ask another question(y/n)? ')
+   if q=='n':
+       break
+   elif q=='y':
+       query=raw_input('You: ')
+       print 'Bot: '+wolfram(query)
+   else:
+       print 'Invalid input. Please try again'
+       q='y'
+       
+       
+        
+    
+
+
